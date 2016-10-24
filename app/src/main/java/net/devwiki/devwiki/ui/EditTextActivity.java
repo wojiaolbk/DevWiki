86994210223b09c6fb53a5e592c3357bba80b22f
DevWiki <674952098@qq.com>
2016-10-24 17:59:35 +0800

    修改风格

--- app/src/main/java/net/devwiki/devwiki/ui/EditTextActivity.java
+++ app/src/main/java/net/devwiki/devwiki/ui/EditTextActivity.java
@@ -1,7 +1,11 @@
 package net.devwiki.devwiki.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import net.devwiki.devwiki.R;
 
@@ -11,5 +15,7 @@ public class EditTextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
