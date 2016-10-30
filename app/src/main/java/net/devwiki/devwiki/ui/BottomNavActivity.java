e904a22f538cd485c4c8fb32550ca15e8837db2f
Asia <bozhou2010@live.com>
2016-10-30 23:13:32 +0800

    修改主题

--- app/src/main/java/net/devwiki/devwiki/ui/BottomNavActivity.java
+++ app/src/main/java/net/devwiki/devwiki/ui/BottomNavActivity.java
@@ -3,7 +3,6 @@ package net.devwiki.devwiki.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
